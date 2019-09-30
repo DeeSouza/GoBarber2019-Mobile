65199f97a2e6950c4331a3b2604e066e395d0eea
Diego Souza <dee-souza@hotmail.com>
2019-09-30 13:37:43 -0300

    Chore: Change storage api for persist reducers

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -4,7 +4,7 @@ import reactotronSaga from 'reactotron-redux-saga';
 
 if (__DEV__) {
 	const tron = Reactotron.configure({
-		host: '192.168.0.125',
+		host: '192.168.0.115',
 	})
 		.useReactNative()
 		.use(reactotronRedux())
